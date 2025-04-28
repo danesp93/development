e84290fc047ad6d1630031fe95cc4b8ac362825f
[danesp93] <[espitiadaniela57@hotmail.com]>
2025-04-28 15:45:50 -0500

    DEVELOPMENT

--- Article preview component/main.js
+++ Article preview component/main.js
@@ -1,16 +1,3 @@
-/*document.querySelector(".imgshare").addEventListener('click', function() {
-  const menu = document.getElementById('shareMenu');
-
-  menu.style.visibility = menu.style.visibility === 'visible' ? 'hidden' : 'visible';
-
-  let colorElement = document.querySelector(".imgshare");
-  colorElement.style.backgroundColor = colorElement.style.backgroundColor === "var(--DBlue)" ? "var(--LiBlue)" : "var(--DBlue)";
-
-  let filterImg = document.querySelector(".imgshare img");
-  filterImg.style.filter = filterImg.style.filter === "grayscale(1) brightness(2)" ? "none" : "grayscale(1) brightness(2)";
-  
-});*/
-
 document.querySelector(".imgshare").addEventListener('click', function() {
   const menu = document.getElementById('shareMenu');
   const img = this.querySelector("img");
